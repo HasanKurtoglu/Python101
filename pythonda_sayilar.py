d0803b76332f0f51dcf7233531c5e32a932406f0
Hasan Kurtoğlu <hasankurtoglu6714@gmail.com>
2023-12-13 20:28:00 +0300

    pythonda sayılar hakkında ufak denemeler

---
+++ pythonda_sayilar.py
@@ -0,0 +1,32 @@
+# print("0,1,2,3,4,5,6,7,8,9")
+
+#For Döngüsü
+#Bu for döngüsü ekrana 10 kere selam yazar
+# for i in range(0,10):
+#     print("Selam")
+    
+    
+#Açıklama Satırı
+#Açıklama Satırı Yapacağın satırları seçip CTRL + K + C basarsan hepsi açıklama satırı olur
+
+
+# sayi = input()
+# for i in range(0,int(sayi)):
+#     print("selam")
+    
+# a = 3
+# b = 4
+# c = a + 2*b
+
+# print(f"a = {a}, b, c ={c}")
+
+# adi = input("öğrenci adını giriniz: ")
+# soyadi = input("öğrenci soyadını giriniz: ")
+# yas = input("öğrenci yaşını giriniz: ")
+# ogrenci = print(f"{adi} {soyadi}, {yas} yaşında")
+
+a = 0
+while a < 10:
+    a += 1
+    print(a)
+    
